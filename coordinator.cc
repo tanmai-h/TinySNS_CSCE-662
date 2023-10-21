aecd6d755499f84866c9467e6bc9577600827fbf
Tanmai Harish <11872828+tanmai-h@users.noreply.github.com>
2023-10-21 16:49:38 +0000

    coordinator

---
+++ coordinator.cc
@@ -0,0 +1,180 @@
+#include <ctime>
+
+#include <google/protobuf/timestamp.pb.h>
+#include <google/protobuf/duration.pb.h>
+#include <chrono>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <stdlib.h>
+#include <unistd.h>
+#include <google/protobuf/util/time_util.h>
+#include <grpc++/grpc++.h>
+#include<glog/logging.h>
+
+#include "coordinator.grpc.pb.h"
+
+using google::protobuf::Timestamp;
+using google::protobuf::Duration;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using csce438::CoordService;
+using csce438::ServerInfo;
+using csce438::Confirmation;
+using csce438::ID;
+
+#define log(severity, msg) LOG(severity) << msg; google::FlushLogFiles(google::severity); 
+
+struct zNode{
+  int serverID;
+  std::string hostname = "localhost";
+  std::string port;
+  std::string type;
+  std::time_t last_heartbeat;
+  bool missed_heartbeat;
+  bool isActive();
+};
+
+std::mutex v_mutex;
+std::vector<zNode> cluster1;
+std::vector<zNode> cluster2;
+std::vector<zNode> cluster3;
+
+std::unordered_map<int, std::vector<zNode>> routingTable = {};
+
+//func declarations
+int findServer(std::vector<zNode> v, int id);
+std::time_t getTimeNow();
+void checkHeartbeat();
+
+bool zNode::isActive(){
+    bool status = false;
+    if(!missed_heartbeat){
+        status = true;
+    }else if(difftime(getTimeNow(),last_heartbeat) < 10){
+        status = true;
+    }
+    return status;
+}
+
+class CoordServiceImpl final : public CoordService::Service {
+
+  Status Heartbeat(ServerContext* context, const ServerInfo* serverInfo, Confirmation* confirmation) override {
+    log(INFO, " heartbeat request " + serverInfo->clusterid());
+      int cid = atoi(serverInfo->clusterid().c_str());
+      if (routingTable.find(cid) == routingTable.end()) {
+        confirmation->set_status(false);
+        log(INFO, "Invalid cid: " + std::to_string(cid));
+        return grpc::Status(grpc::StatusCode::NOT_FOUND, std::string("Cluster ID: ") + std::to_string(serverInfo->serverid()) + std::string(" not found"));
+      }
+      auto znode = routingTable[cid][0];
+      znode.serverID = serverInfo->serverid();
+      znode.port = serverInfo->port();
+      znode.type = serverInfo->type();
+
+      sleep(5);
+      znode.last_heartbeat = getTimeNow();
+      log(INFO, "Got Heartbeat from clusterId, serverId: " + std::string(serverInfo->clusterid()) + std::string(",") + std::to_string(serverInfo->serverid()));
+      confirmation->set_status(true);
+
+    return Status::OK;
+  }
+  
+  Status GetServer(ServerContext* context, const ID* id, ServerInfo* serverInfo) override {
+    std::cout<<"Got request for clientID: "<<id->id()<<std::endl;
+    int clusterID = (id->id()-1)%3+1;
+    if (routingTable.find(clusterID) != routingTable.end()) {
+      std::vector<zNode> cluster = routingTable[clusterID];
+      for (auto c : cluster) {
+        if (c.isActive()) {
+          serverInfo->set_serverid(c.serverID);
+          serverInfo->set_hostname(c.hostname);
+          serverInfo->set_port(c.port);
+          serverInfo->set_type(c.type);
+          break;
+        } else {
+          return grpc::Status(grpc::StatusCode::UNAVAILABLE, "Server Not Available");
+        }
+      }
+    } else {
+      return grpc::Status(grpc::StatusCode::NOT_FOUND, "Clusted info not present in routing table");
+    }
+    return Status::OK;
+  }
+};
+
+void RunServer(std::string port_no){
+  //start thread to check heartbeats
+  std::thread hb(checkHeartbeat);
+  //localhost = 127.0.0.1
+  std::string server_address("127.0.0.1:"+port_no);
+  CoordServiceImpl service;
+  //grpc::EnableDefaultHealthCheckService(true);
+  //grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  zNode z1,z2,z3;
+  std::vector<zNode> s1={z1},s2={z2},s3={z3};
+  routingTable[1] = s1;
+  routingTable[2] = s2;
+  routingTable[3] = s3;
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  
+  std::string port = "9090";
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "p:")) != -1){
+    switch(opt) {
+      case 'p':
+          port = optarg;
+          break;
+      default:
+	std::cerr << "Invalid Command Line Argument\n";
+    }
+  }
+  RunServer(port);
+  return 0;
+}
+
+void checkHeartbeat() {
+  while (true) {
+      for (auto& clusterPair : routingTable) {
+          for (zNode& server : clusterPair.second) {
+              if (server.isActive() && difftime(getTimeNow(), server.last_heartbeat) > 10) {
+                  server.missed_heartbeat = true;
+              } else if (!server.isActive() && difftime(getTimeNow(), server.last_heartbeat) <= 10) {
+                  server.missed_heartbeat = false;
+              }
+          }
+      }
+      sleep(3);  // Sleep for 3 seconds before checking again
+  }
+}
+
+std::time_t getTimeNow(){
+    return std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+}
---
+++ tsc.cc
@@ -0,0 +1,364 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <csignal>
+#include <grpc++/grpc++.h>
+#include "client.h"
+#include "coordinator.grpc.pb.h"
+#include "sns.grpc.pb.h"
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+using csce438::Message;
+using csce438::ListReply;
+using csce438::Request;
+using csce438::Reply;
+using csce438::SNSService;
+using csce438::CoordService;
+using csce438::ServerInfo;
+using csce438::Confirmation;
+using csce438::ID;
+
+Message MakeMessage(const std::string& username, const std::string& msg) {
+    Message m;
+    m.set_username(username);
+    m.set_msg(msg);
+    google::protobuf::Timestamp* timestamp = new google::protobuf::Timestamp();
+    timestamp->set_seconds(time(NULL));
+    timestamp->set_nanos(0);
+    m.set_allocated_timestamp(timestamp);
+    return m;
+}
+
+class Client : public IClient {
+public:
+  Client(const std::string& hname,
+	 const std::string& uname,
+	 const std::string& p)
+    :cip(hname), uid(uname), cport(p) {}
+
+  
+protected:
+  virtual int connectTo();
+  virtual IReply processCommand(std::string& input);
+  virtual void processTimeline();
+
+private:
+  std::string cip;
+  std::string uid;
+  std::string cport;
+  
+  // You can have an instance of the client stub
+  // as a member variable.
+  std::unique_ptr<SNSService::Stub> stub_;
+  std::unique_ptr<CoordService::Stub> coordStub;
+
+  IReply Login();
+  IReply List();
+  IReply Follow(const std::string &username);
+  IReply UnFollow(const std::string &username);
+  void   Timeline(const std::string &username);
+};
+
+int Client::connectTo() {
+    std::string login_info = cip + ":" + cport;
+  
+    coordStub = CoordService::NewStub(grpc::CreateChannel(
+          login_info, grpc::InsecureChannelCredentials()
+        ));
+
+    ClientContext clientContext;
+    ServerInfo serverInfo;
+    ID id;
+    // id.set_id(atoi(uid.c_str()));
+    grpc::Status grpcStatus = coordStub->GetServer(&clientContext,id,&serverInfo);
+    if (!grpcStatus.ok()) {
+      return -1;
+    }
+
+    login_info = serverInfo.hostname() + ":" + serverInfo.port();
+
+    stub_ = std::unique_ptr<SNSService::Stub>(SNSService::NewStub(
+        grpc::CreateChannel(
+        login_info, grpc::InsecureChannelCredentials())));
+    
+    IReply ire = Login();
+    if(!ire.grpc_status.ok() || (ire.comm_status == FAILURE_ALREADY_EXISTS)) {
+      return -1;
+    }
+    return 1;
+}
+
+IReply Client::processCommand(std::string& input)
+{
+  // ------------------------------------------------------------
+  // GUIDE 1:
+  // In this function, you are supposed to parse the given input
+  // command and create your own message so that you call an 
+  // appropriate service method. The input command will be one
+  // of the followings:
+  //
+  // FOLLOW <username>
+  // UNFOLLOW <username>
+  // LIST
+  // TIMELINE
+  //
+  // - JOIN/LEAVE and "<username>" are separated by one space.
+  // ------------------------------------------------------------
+  
+  // ------------------------------------------------------------
+  // GUIDE 2:
+  // Then, you should create a variable of IReply structure
+  // provided by the client.h and initialize it according to
+  // the result. Finally you can finish this function by returning
+  // the IReply.
+  // ------------------------------------------------------------
+  
+  
+  // ------------------------------------------------------------
+  // HINT: How to set the IReply?
+  // Suppose you have "Join" service method for JOIN command,
+  // IReply can be set as follow:
+  // 
+  //     // some codes for creating/initializing parameters for
+  //     // service method
+  //     IReply ire;
+  //     grpc::Status status = stub_->Join(&context, /* some parameters */);
+  //     ire.grpc_status = status;
+  //     if (status.ok()) {
+  //         ire.comm_status = SUCCESS;
+  //     } else {
+  //         ire.comm_status = FAILURE_NOT_EXISTS;
+  //     }
+  //      
+  //      return ire;
+  // 
+  // IMPORTANT: 
+  // For the command "LIST", you should set both "all_users" and 
+  // "following_users" member variable of IReply.
+  // ------------------------------------------------------------
+
+  IReply ire;
+  std::size_t index = input.find_first_of(" ");
+  std::cout << "Processing "+input + ". ";
+  if (index != std::string::npos) {
+    std::string cmd = input.substr(0, index);
+    
+    /*
+      if (input.length() == index + 1) {
+      std::cout << "Invalid Input -- No Arguments Given\n";
+      }
+    */
+    
+    std::string argument = input.substr(index+1, (input.length()-index));
+    
+    if (cmd == "FOLLOW") {
+      return Follow(argument);
+    } else if(cmd == "UNFOLLOW") {
+      return UnFollow(argument);
+    }
+  } else {
+    if (input == "LIST") {
+      return List();
+    } else if (input == "TIMELINE") {
+      ire.comm_status = SUCCESS;
+      return ire;
+    }
+  }
+  
+  ire.comm_status = FAILURE_INVALID;
+  return ire;
+}
+
+void Client::processTimeline()
+{
+    Timeline(uid);
+    // ------------------------------------------------------------
+    // In this function, you are supposed to get into timeline mode.
+    // You may need to call a service method to communicate with
+    // the server. Use getPostMessage/displayPostMessage functions
+    // for both getting and displaying messages in timeline mode.
+    // You should use them as you did in hw1.
+    // ------------------------------------------------------------
+
+    // ------------------------------------------------------------
+    // IMPORTANT NOTICE:
+    //
+    // Once a user enter to timeline mode , there is no way
+    // to command mode. You don't have to worry about this situation,
+    // and you can terminate the client program by pressing
+    // CTRL-C (SIGINT)
+    // ------------------------------------------------------------
+
+}
+
+///////////////////////////////////////////
+// List Command
+//////////////////////////////////////////
+IReply Client::List() {
+    //Data being sent to the server
+    Request request;
+    request.set_username(uid);
+
+    //Container for the data from the server
+    ListReply list_reply;
+
+    //Context for the client
+    ClientContext context;
+
+    Status status = stub_->List(&context, request, &list_reply);
+    IReply ire;
+    ire.grpc_status = status;
+
+    //Loop through list_reply.all_users and list_reply.following_users
+    //Print out the name of each room
+    if(status.ok()){
+        ire.comm_status = SUCCESS;
+        std::string all_users;
+        std::string following_users;
+        for(std::string s : list_reply.all_users()){
+            ire.all_users.push_back(s);
+        }
+        for(std::string s : list_reply.followers()){
+            ire.followers.push_back(s);
+        }
+    }
+    return ire;
+}
+        
+IReply Client::Follow(const std::string& username2) {
+    Request request;
+    request.set_username(uid);
+    request.add_arguments(username2);
+
+    Reply reply;
+    ClientContext context;
+
+    Status status = stub_->Follow(&context, request, &reply);
+    IReply ire; ire.grpc_status = status;
+    if (reply.msg() == "unkown user name") {
+        ire.comm_status = FAILURE_INVALID_USERNAME;
+    } else if (reply.msg() == "unknown follower username") {
+        ire.comm_status = FAILURE_INVALID_USERNAME;
+    } else if (reply.msg() == "you have already joined") {
+        ire.comm_status = FAILURE_ALREADY_EXISTS;
+    } else if (reply.msg() == "Follow Successful") {
+        ire.comm_status = SUCCESS;
+    } else {
+        ire.comm_status = FAILURE_UNKNOWN;
+    }
+    return ire;
+}
+
+IReply Client::UnFollow( const std::string& username2) {
+    Request request;
+
+    request.set_username(uid);
+    request.add_arguments(username2);
+
+    Reply reply;
+
+    ClientContext context;
+
+    Status status = stub_->UnFollow(&context, request, &reply);
+    IReply ire;
+    ire.grpc_status = status;
+    if (reply.msg() == "Unknown follower") {
+        ire.comm_status = FAILURE_INVALID_USERNAME;
+    } else if (reply.msg() == "You are not a follower") {
+        ire.comm_status = FAILURE_NOT_A_FOLLOWER;
+    } else if (reply.msg() == "UnFollow Successful") {
+        ire.comm_status = SUCCESS;
+    } else {
+        ire.comm_status = FAILURE_UNKNOWN;
+    }
+
+    return ire;
+}
+
+IReply Client::Login() {
+    Request request;
+    request.set_username(uid);
+    Reply reply;
+    ClientContext context;
+
+    Status status = stub_->Login(&context, request, &reply);
+
+    IReply ire;
+    ire.grpc_status = status;
+    std::cout << "REPLY MESSAGE: " + reply.msg();
+    if (reply.msg() == "you have already joined") {
+        ire.comm_status = FAILURE_ALREADY_EXISTS;
+    } else {
+        ire.comm_status = SUCCESS;
+    }
+    return ire;
+}
+
+void Client::Timeline(const std::string& username) {
+  ClientContext context;
+
+  std::shared_ptr<ClientReaderWriter<Message, Message>> stream(
+				  stub_->Timeline(&context));
+
+  //Thread used to read chat messages and send them to the server
+  std::thread writer([username, stream]() {
+    std::string input = "Set Stream";
+    Message m = MakeMessage(username, input);
+    stream->Write(m);
+    while (1) {
+      input = getPostMessage();
+      m = MakeMessage(username, input);
+      stream->Write(m);
+    }
+    stream->WritesDone();
+  });
+  
+  std::thread reader([username, stream]() {
+    Message m;
+    while(stream->Read(&m)){
+      google::protobuf::Timestamp temptime = m.timestamp();
+      std::time_t time = temptime.seconds();
+      displayPostMessage(m.username(), m.msg(), time);
+    }
+  });
+  
+  //Wait for the threads to finish
+  writer.join();
+  reader.join();
+}
+
+int main(int argc, char** argv) {
+
+  std::string cip = "127.0.0.1";
+  std::string cport = "9090";
+  std::string uid = "1";
+    
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "h:k:u:")) != -1){
+    switch(opt) {
+    case 'h':
+      cip = optarg;break;
+    case 'k':
+      cport = optarg;break;
+    case 'u':
+      uid = optarg;break;
+    default:
+      std::cout << "Invalid Command Line Argument\n";
+    }
+  }
+
+  std::cout << "Logging Initialized. Client starting...";
+  
+  Client myc(cip, cport, cport);
+
+  myc.run();
+  
+  return 0;
+}
