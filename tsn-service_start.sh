77e4fbdd1104a190a479706e94a7d9ce9e26af09
Tanmai Harish <11872828+tanmai-h@users.noreply.github.com>
2023-10-21 16:51:55 -0500

    Update tsn-service_start.sh

--- tsn-service_start.sh
+++ tsn-service_start.sh
@@ -1,5 +1,4 @@
-./coordinator -p 9090
-./tsd -c 1 -s 1 -h localhost -k 9090 -p 10000
-./tsd  -c 2 -s 1 -h localhost -k 9090 -p 10001
-./tsc -h localhost -k 9090 -u 1
-./tsc -h localhost -k 9090 -u 2
+./coordinator -p 9090 &
+sleep 1s
+./tsd -c 1 -s 1 -h localhost -k 9090 -p 10000 &
+./tsd  -c 2 -s 1 -h localhost -k 9090 -p 10001 &
